59356c0c9192a44402e402f9ce592f6e8b4893fe
Filipe Versani <filipeversani99@gmail.com>
2024-01-27 20:14:00 -0300

    Initial commit

---
+++ adivinhacao.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h> 
+
+
+
+int main() {
+    printf("****************************************** \n");
+    printf("* Bem-vindo ao nosso jogo de adivinhacao * \n");
+    printf("****************************************** \n");
+
+    int segundos = time(0);  // faz com que um numero randomico realmente fique randomico
+    srand(segundos); 
+
+    int numerogrande = rand(); // gera um numero randomico 
+
+    int numerosecreto = numerogrande % 100;
+    int chute;
+    int tentativas = 1;
+
+    double pontos = 1000;
+
+    while(1) {
+
+        printf("Tentativa %d\n", tentativas);
+        printf("Qual e o seu chute? ");
+
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+
+        if(chute < 0) {
+            printf("Voce nao pode chutar numeros negativos!\n");
+            continue;
+        }
+
+    int acertou = (chute == numerosecreto);
+    int maior = chute > numerosecreto;
+
+    if(acertou) {
+        printf("Parabens! Vocr acertou!\n");
+        printf("Jogue de novo, voce e um bom jogador!\n");
+
+        break;
+    }
+
+            else if(maior) {
+                printf("Seu chute foi maior que o numero secreto\n");
+            }
+
+            else {
+                printf("Seu chute foi menor que o numero secreto\n");
+            }
+
+            tentativas++;
+
+            double pontosperdidos = abs(chute - numerosecreto) / 2.0;
+          
+            pontos = pontos - pontosperdidos;
+
+        }
+
+        printf("Fim de jogo!\n");
+        printf("Voce acertou em %d tentativas!\n", tentativas);
+        printf("Total de pontos: %.1f\n", pontos);
+
+}
